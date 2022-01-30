00c93ec23255ea9ddbcb6fcb0e6b236ef30c893a
chtc <notnotcha0t1c@protonmail.com>
2022-01-30 18:03:42 +0100

    cmus: change colors so cmus fits in more
    neovim(other): reload xresources on change
    
    xresources: make st padding bigger

--- .config/nvim/lua/other.lua
+++ .config/nvim/lua/other.lua
@@ -4,6 +4,8 @@ local cmd = vim.cmd
 cmd [[ au BufWritePre * %s/\s\+$//e ]]
 cmd [[ au BufWritePre * %s/\n\+\%$//e ]]
 
+cmd [[ au BufWritePost Xresources !xrdb -load ~/.config/x11/Xresources ]]
+
 -- Active statusline
 cmd [[ au WinEnter * setlocal statusline=%!luaeval('statusline()') ]]
 -- Inactive statusline
