c7b5f8f52015f15f1e52871b8b012365742993e1
chtc <notnotcha0t1c@protonmail.com>
2022-04-14 14:33:48 +0200

    nvim(treesitter): add markdown parser

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 ts_config.setup {
     -- Ensure parsers are installed for these languages
-    ensure_installed = { "lua", "css", "html" },
+    ensure_installed = { "lua", "css", "html", "markdown" },
     -- Don't install the parser for C
     ignore_install = { "c" },
     -- Enable highlighting
