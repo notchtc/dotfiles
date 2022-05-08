7de2f83c6bbafd177464d48b1f9c54dab05a7f1c
chtc <notnotcha0t1c@protonmail.com>
2022-05-08 16:24:48 +0200

    polybar(music_notif): add cover art
    polybar(music_notif,cmus): fix bug with title
    
    dunst: change width and max icon size
    
    polybar(battery,music): add _notif to filename

--- .config/polybar/cmus.sh
+++ .config/polybar/cmus.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-status=$(cmus-remote -Q | grep status | cut -c8-)
-title=$(cmus-remote -Q | grep title | cut -c11-)
+status=$(cmus-remote -Q | grep -m1 '^status' | cut -c8-)
+title=$(cmus-remote -Q | grep -m1 '^tag title' | cut -c11-)
 
 if [ "$status" = "playing" ]; then
     icon=""
