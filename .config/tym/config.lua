e44558e5c3f4bd9c3a04d841790ccfc2e122bcea
chtc <notnotcha0t1c@protonmail.com>
2022-05-23 18:31:01 +0200

    herbstluftwm: fix music and rss keybinds

--- .config/tym/config.lua
+++ .config/tym/config.lua
@@ -1,9 +1,9 @@
-local tym = require("tym")
+local tym = require "tym"
 
-tym.set_config({
+tym.set_config {
     font = "Iosevka Nerd Font Mono 12",
     cursor_shape = "ibeam",
     cursor_blink_mode = "on",
     padding_horizontal = 6,
     padding_vertical = 6,
-})
+}
