ee55b2ed8f30ea7417bff66cb21225f04eedc898
chtc <notnotcha0t1c@protonmail.com>
2022-05-23 18:24:21 +0200

    everything: switch to tym from st

---
+++ .config/tym/config.lua
@@ -0,0 +1,9 @@
+local tym = require("tym")
+
+tym.set_config({
+    font = "Iosevka Nerd Font Mono 12",
+    cursor_shape = "ibeam",
+    cursor_blink_mode = "on",
+    padding_horizontal = 6,
+    padding_vertical = 6,
+})
